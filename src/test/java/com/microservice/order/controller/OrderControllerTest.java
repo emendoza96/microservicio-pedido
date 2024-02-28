558026118d493485e335fb01018e4bd42af1a2c0
emendoza96 <emilianomendoza294@gmail.com>
2024-02-28 20:36:42 -0300

    Fix mock mvc xml responses

--- src/test/java/com/microservice/order/controller/OrderControllerTest.java
+++ src/test/java/com/microservice/order/controller/OrderControllerTest.java
@@ -19,6 +19,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -64,6 +65,7 @@ public class OrderControllerTest {
 
         mockMvc = MockMvcBuilders.standaloneSetup(orderController)
             .addFilters(new MockJwtAuthorizationFilter())
+            .setMessageConverters(new MappingJackson2HttpMessageConverter())
             .build()
         ;
 
